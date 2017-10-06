079d0c18f14b2ef16053015875cff73d9c0bfa2a
Taoge <wenter.wu@gmail.com>
2017-10-06 11:31:28 +0800

    + event models

--- imuseum/imuseum_crawler/README.rst
+++ imuseum/imuseum_crawler/README.rst
@@ -1,13 +1,14 @@
 IMuseum  爬虫
 ===========================
 
-
-## Design
+Design
+--------------------
 
 爬虫参考了 Scrapy, 数据流与控制流分离。使用了 asyncio 异步非阻塞的IO 库。
 
 
-### Components
+Components
+-------------------
 
 各部件的行为参考 https://docs.scrapy.org/en/latest/topics/architecture.html
 
@@ -18,7 +19,8 @@ IMuseum  爬虫
 
 
 
-## 爬虫分析
+ 爬虫分析
+----------------------
 
 参考 `parser.rst`
 
--- imuseum/imuseum_crawler/models.py
+++ imuseum/imuseum_crawler/models.py
@@ -10,15 +10,49 @@ class Museum(Model):
     pass
 
 
-class Show(Model):
-    pass
+class Event(Model):
+    fields = [
+        {
+            "key": "title",
+            "type": "string"
+        },
+        {
+            "key": "subtitle",
+            "type": "int"
+        },
+        {
+            "key": "address",
+            "type": "string"
+        },
+        {
+            "key": "summary",
+            "type": "string"
+        },
+        {
+            "key": "description",
+            "type": "html"
+        },
+        {
+            "key": "when",
+            "type": "tuple"
+        },
+        {
+            "key": "museum",
+            "type": "string"
+        },
+        {
+            "key": "price",
+            "type": "tuple"
+        }
+
+    ]
 
 
-class People(Model):
-    pass
+class User(Model):
+    fields = [
+        
+    ]
 
 
 class Activity(Model):
     pass
-
-
